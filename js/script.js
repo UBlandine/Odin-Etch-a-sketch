7aa07b7025c702158bd6fbae56fc42a6ea2f5b3a
UBlandine <ugirablando@gmail.com>
2023-07-20 18:39:35 +0200

    Etch-A-Sketch

--- js/script.js
+++ js/script.js
@@ -1,53 +1,86 @@
-document.addEventListener("DOMContentLoaded", function(){
+let color = "black";
+let click = true;
+let gridSize = 16;
 
-  createBoard(16);
+// Function to populate the board with squares and attach event listeners
+function populateBoard(size){
+ gridSize = size;
+let board = document.querySelector('.board');
+board.innerHTML = ""; // clear existing squares
+// let squares = board.querySelectorAll('div');
+// squares.forEach((div) => div.remove());
+board.style.gridTemplateColumns = "repeat($(size) , 1fr)";
+board.style.gridTemplateRows = "repeat($(size) , 1fr)";
 
-  let btn_popup = document.querySelector("#popup");
-  btn_popup.addEventListener("click", function(){
-    let size = getSize();
-    createBoard(size);
-  })
+let amount = size * size;
 
-})
 
-function createBoard(size){
-  let board = document.querySelector(".board");
-  board.style.gridTemplateColumns = "repeat(${size}, 1fr)";
-  board.style.gridTemplateRows = "repeat(${size}, 1fr";
+for(let i = 0; i< amount; i++){
+ let square = document.createElement("div");
+ // square.style.width = `${squareSize}%`;
+ // square.style.height = `${squareSize}%`;
+ square.style.backgroundColor = "skyblue";
+  square.addEventListener("mouseover", colorSquare);
+  board.appendChild(square);
+ // board.insertAdjacentElement("beforeend", square);
+}
+}
 
-  let numDivs = size * size;
+populateBoard(gridSize);
 
-  for(let i = 0; i< numDivs; i++){
-    let div = document.createElement("div");
-   div.addEventListener("mouseover", colorDic) {
-    
-    board.insertAdjacentElement("beforeend", div);
+function changeSize(input){
+ if (input >= 2 && input <= 100){
+   document.querySelector('.error').style.display = 'none';
+   gridSize = input;
+ }
+ else {
+   document.querySelector('.error').style.display = 'flex';
+ 
+ }
+ }
+
+ // Function to set the background color of the square when the mouse hovers over it
+ function colorSquare() {
+   if (click){
+   if (color === "random") {
+   this.style.backgroundColor = `hsl(${Math.random() * 300}, 100%, 50%)`;
+
+ } else if (color === "erasor") {
+   this.style.backgroundColor = "white"; // Erasor mode, set the color to white
+ }
+ 
+ else {
+   this.style.backgroundColor = color;
+ }
+}
+}
+ function changeColor(choice) {
+   color = choice;
+ }
+
+ function resetBoard() {
+   let board = document.querySelector('.board');
+   let squares = board.querySelectorAll('div');
+squares.forEach((div) => (div.style.backgroundColor = "skyblue"));
+
+ }
+
+ function setBoardSize() {
+   if (gridSize >= 2 && gridSize <= 100) {
+     populateBoard(gridSize);
+   } else {
+     document.querySelector('>error').style.display = 'flex';
    }
-}
+ }
 
-function getSize(){
-  let inPut = prompt("What will bw the size of the board");
-  if(input == ""){
-    message.innerHtml = " please provide a number";
+ document.querySelector("body").addEventListener("click", (e) => {
+   if (e.target.tagName !== 'BUTTON'){
 
-  }
-  else if (input < 0 || input > 100){
-    message.innerHtml = "provide a number between 1 and 100"
-  }
-   else{
-    message.innerHtml = "Now yo u play"
-    return input;
+   click = !click;
+   if(click) {
+     document.querySelector('.mode').textContent = "Mode : Coloring";
+   } else{
+     document.querySelector('.mode').textContent = "Mode : Not Coloring";
    }
-function colorDiv(){
-if(color == "random"){
-  this.style.backgroundColor = 'hsl(${Math.random() * 360}, 100%, 50%)'
-}
-else{
-  this.style.backgroundColor = 'black'
-}
-}
-function setColor(colorChoice){
-  let color = colorChoice;
-}
-
-}
+ } 
+ });
